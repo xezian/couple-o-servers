860884b7af26e2b152bb19a9e96669cecc4c5602
xezian <xezian@icloud.com>
2018-01-27 12:49:43 -0700

    server served html pages

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT_ONE = 7000;
-const PORT_TWO = 7500;
+const PORT_TWO = 8080;
 
 function kindHandler(request, response) {
     response.end("Let me just tell you this: You are a sparkly rainbow of pure magnificence");
 };
-function unkindHandler(request, response) {
-    response.end("WOWEEE. You are so dumb.");
+function otherHandler(request, response) {
+    let path = request.url;
+    switch(path) {
+        case "/":
+            path = path + "index.html";
+            return displayFile(path, request, response);
+        case "/foods":
+            path = path + ".html";
+            return displayFile(path, request, response);
+        case "/movies":
+            path = path + ".html";
+            return displayFile(path, request, response);
+        case "/CSSfaves":
+            path = path + ".html";
+            return displayFile(path, request, response);
+        default:
+            path = "/404.html";
+            return displayFile(path, request, response);
+    }
 };
+function displayFile(path, request, response) {
+    fs.readFile(__dirname + path, function(err, data) {
+        if(path = "/404.html") {
+            response.writeHead(404, {"Content-Type":"text.html"});
+            response.end(data);
+        } else {
+        response.writeHead(200, {"Content-Type":"text.html"});
+        response.end(data);
+        }
+    });
+};
+
 const serverOne = http.createServer(kindHandler);
-const serverTwo = http.createServer(unkindHandler);
+const serverTwo = http.createServer(otherHandler);
 
 serverOne.listen(PORT_ONE, function() {
     console.log("Server listening on: http://localhost:" + PORT_ONE);
